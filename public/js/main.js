9d3d2caaa8c920ad3ae4f3251bbd594bd0ca6c89
maliksaepul <malik@inspigo.id>
2021-03-26 00:08:31 +0700

    love notes

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+
+document.getElementsByClassName("content_challenge__post-more")[0].addEventListener('click', function() {
+    document.getElementsByClassName("content_challenge__post-texts")[0].classList.toggle("content_challenge__post-notexpanded")    
+})
+
+document.getElementsByClassName("content_banner")[0].addEventListener('click', function () {
+    window.open('https://www.bit.ly/perempuanmusisiidolaku', '_blank');
+})
+
